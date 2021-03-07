6d0f749602db46f0e2877f53bdef5de7b4eb9b52
Naresh-badisa <naresh9badisa@gmail.com>
2021-03-07 19:02:32 +0530

    supermarketmanagementproject

---
+++ src/main/java/com/cts/SuperMarketManagementA039Application.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarketManagementA039Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMarketManagementA039Application.class, args);
+	}
+
+}
